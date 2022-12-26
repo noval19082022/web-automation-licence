e1ae7681749d46903f75a1955458f3a15e40beef
Sakti <sakti@mamiteam.com>
2022-12-26 15:55:05 +0700

    Hooks added

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,33 @@
+package steps;
+
+import com.microsoft.playwright.Tracing;
+import config.browser.FrameworkConfig;
+import config.browser.FrameworkInitialize;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import java.nio.file.Paths;
+
+public class Hooks {
+    @Before
+    public void setup(Scenario scenario) {
+        FrameworkConfig.localPage = new FrameworkInitialize().initializePlaywright();
+        System.out.println(scenario.getName() + " is started");
+    }
+
+    @After
+    public void cleanUp(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshotBytes = FrameworkConfig.localPage.screenshot();
+            scenario.attach(screenshotBytes, "image/png", scenario.getName());
+        }
+        FrameworkConfig.localBrowserContext.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("target/trace/"+scenario.getName().trim()+"-trace.zip")));
+        FrameworkConfig.localPage.close();
+        FrameworkConfig.localBrowser.close();
+        FrameworkConfig.localBrowserContext.close();
+        FrameworkConfig.localPlaywright.close();
+        System.out.println(scenario.getName() + " is finished");
+    }
+}
