5119ef2e0eb1af55190260413c93597fc279abc6
Sakti <sakti@mamiteam.com>
2023-01-09 10:46:47 +0700

    Set context options to max screen size

--- src/main/java/config/global/GlobalConfig.java
+++ src/main/java/config/global/GlobalConfig.java
@@ -3,12 +3,14 @@ package config.global;
 import utilities.JavaHelpers;
 
 public class GlobalConfig {
-    private static final String PROPERTYFILE="src/main/resources/global-config.properties";
-    public static final String ENV = JavaHelpers.getPropertyValue(PROPERTYFILE, "env");
-    public static final String BROWSER_NAME = JavaHelpers.getPropertyValue(PROPERTYFILE, "browserName");
-    public static final boolean HEADLESS = Boolean.parseBoolean(JavaHelpers.getPropertyValue(PROPERTYFILE, "headless"));
-    public static final boolean DEV_TOOLS = Boolean.parseBoolean(JavaHelpers.getPropertyValue(PROPERTYFILE, "devTools"));
-    public static final double SLOW_MO = Double.parseDouble(JavaHelpers.getPropertyValue(PROPERTYFILE, "slowMo"));
-    public static final boolean SET_ACCEPT_DOWNLOAD = Boolean.parseBoolean(JavaHelpers.getPropertyValue(PROPERTYFILE, "setAcceptDownload"));
-    public static final boolean SET_VIDEO_RECORD = Boolean.parseBoolean(JavaHelpers.getPropertyValue(PROPERTYFILE, "setVideoRecord"));
+    private static final String GLOBAL_CONFIG_PROPERTIES ="src/main/resources/global-config.properties";
+    public static final String BROWSER_NAME = JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "browserName");
+    public static final boolean HEADLESS = Boolean.parseBoolean(JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "headless"));
+    public static final boolean DEV_TOOLS = Boolean.parseBoolean(JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "devTools"));
+    public static final double SLOW_MO = Double.parseDouble(JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "slowMo"));
+    public static final boolean SET_ACCEPT_DOWNLOAD = Boolean.parseBoolean(JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "setAcceptDownload"));
+    public static final boolean SET_VIDEO_RECORD = Boolean.parseBoolean(JavaHelpers.getPropertyValue(GLOBAL_CONFIG_PROPERTIES, "setVideoRecord"));
+
+    public static final int SCREEN_WIDTH = JavaHelpers.getScreenWidth();
+    public static final int SCREEN_HEIGHT = JavaHelpers.getScreenHeight();
 }
