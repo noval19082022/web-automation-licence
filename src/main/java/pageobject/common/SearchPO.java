ad08056b66d594540c4b0ef6a2c436edbc97d911
Gizkara <gizkarabagas98@gmail.com>
2023-02-21 18:19:18 +0700

    new feature main search

--- src/main/java/pageobject/common/SearchPO.java
+++ src/main/java/pageobject/common/SearchPO.java
@@ -2,6 +2,7 @@ package pageobject.common;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import utilities.PlaywrightHelpers;
 
 public class SearchPO {
     Page page;
@@ -10,20 +11,26 @@ public class SearchPO {
     private Locator searchBoxInput;
     private Locator suggetionKostOnTheSearchList;
     Locator suggestionAreaOnTheSearchList;
+    private PlaywrightHelpers playwright;
+    Locator clearArea;
 
 
     public SearchPO(Page page) {
+        this.playwright = new PlaywrightHelpers(page);
         this.page = page;
         this.inputSearch = page.locator("input[title]");
         this.searchKost = page.getByText("Masukan nama lokasi/area/alamat");
         this.searchBoxInput = page.locator("input[type='search']");
         this.suggetionKostOnTheSearchList = page.getByTestId("suggestionBox-roomList").nth(0);
         this.suggestionAreaOnTheSearchList = page.locator("(//label[@class='results-title'])[1]");
+        this.clearArea = page.locator("//input[@type='search']");
+
 
     }
 
     /**
      * Search kost by test
+     *
      * @param search kost name
      * @return KostDetailsPO
      */
@@ -36,6 +43,7 @@ public class SearchPO {
 
     /**
      * Enter Text in search bar and select result
+     *
      * @param kostName
      */
 
@@ -48,6 +56,7 @@ public class SearchPO {
 
     /**
      * search area by name
+     *
      * @param search
      */
 
@@ -55,5 +64,50 @@ public class SearchPO {
         searchKost.click();
         searchBoxInput.fill(search);
         suggestionAreaOnTheSearchList.click();
+    }
+
+    /**
+     * result area by name
+     *
+     * @return
+     */
+
+    public boolean listResultKeyword(String area) {
+        Locator areaCity = page.getByText(area).nth(1);
+        return areaCity.isVisible();
+    }
+
+
+    /**
+     * search area by keyword
+     *
+     * @param search
+     */
+    public void searchArea(String search) {
+        searchKost.click();
+        searchBoxInput.fill(search);
+        searchBoxInput.press("Enter");
+    }
+
+    /**
+     * area not found by keyword
+     *
+     * @return
+     */
+
+    public boolean setExeptionText(String tidakditemukan) {
+        Locator notfound = page.getByText(tidakditemukan);
+        return notfound.isVisible();
+    }
+
+    /**
+     * searchbar is empty
+     *
+     * @return
+     */
+    public boolean isSearchbarEmpty() {
+        clearArea.clear();
+        clearArea.isVisible();
+        return clearArea.textContent().equals("");
     }
 }
