40c809aa001c7b0bb489347a7534aeb221edda84
Sakti <sakti@mamiteam.com>
2023-02-21 10:55:08 +0700

    remove unused import

--- src/main/java/pageobject/common/KostDetailsPO.java
+++ src/main/java/pageobject/common/KostDetailsPO.java
@@ -2,7 +2,6 @@ package pageobject.common;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
 import pageobject.tenant.BookingFormPO;
 import utilities.JavaHelpers;
 import utilities.LocatorHelpers;
