5c5c0976816adc5931d5a201c0474dcd1b9d3096
destafajri <>
2023-05-02 13:02:04 +0700

    QAT-2313 new branch

--- src/main/java/pageobject/tenant/BookingFormPO.java
+++ src/main/java/pageobject/tenant/BookingFormPO.java
@@ -3,6 +3,9 @@ package pageobject.tenant;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import utilities.PlaywrightHelpers;
+
+import java.nio.file.Paths;
 
 public class BookingFormPO {
     Page page;
@@ -19,6 +22,8 @@ public class BookingFormPO {
     Locator ubahButton;
     Locator selectPayWithDP;
     Locator simpanButton;
+    Locator closeBtn;
+    Locator uploadDoc;
 
     public BookingFormPO(Page page) {
         this.page = page;
@@ -35,6 +40,8 @@ public class BookingFormPO {
         this.ubahButton = page.locator(".booking-payment-option .booking-form-section__action .bg-c-text");
         this.selectPayWithDP = page.locator("[for='bookingFormRadio-paymentSelect-0'] > .bg-c-radio__icon");
         this.simpanButton = page.locator(".bg-c-button--secondary");
+        this.closeBtn = page.getByRole(AriaRole.BUTTON).filter(new Locator.FilterOptions().setHasText("close"));
+        this.uploadDoc = page.locator("div").getByTestId("bookingDocumentUploader").first().locator("//input[@type='file']");
     }
 
     /**
@@ -61,7 +68,47 @@ public class BookingFormPO {
         return new SuccessBookingPO(page);
     }
 
-    ;
+    /**
+     * add jumlah penyewa
+     *
+     * @param penyewa
+     */
+    public void addJumlahPenyewa(int penyewa) {
+        for (int i = 0; i < penyewa; i++) {
+            page.getByTestId("bookingFormTotalRenter-stepper").getByRole(AriaRole.BUTTON).nth(1).click();
+        }
+    }
+
+    /**
+     * @param checkMark example checkmark Membawa anak
+     */
+    public void checkMark(String checkMark) {
+        page.locator("label").filter(new Locator.FilterOptions().setHasText("checkmark " + checkMark)).locator("span").click();
+    }
+
+    /**
+     * Upload berkas KTP, Foto diri KTP, KK, buku nikah
+     */
+    public void uploadBerkasBooking() {
+        PlaywrightHelpers playwright = new PlaywrightHelpers(page);
+        do {
+            String projectpath = System.getProperty("user.dir");
+
+            if (closeBtn.first().isVisible()) {
+                closeBtn.first().click();
+            }
+
+            if (uploadDoc != null) {
+                uploadDoc.setInputFiles(Paths.get(projectpath + "/src/main/resources/images/upload.jpg"));
+                playwright.hardWait(5_000);;
+            }
+
+            if (page.getByText("Pada saat masuk kos, mohon siapkan kartu identitas asli untuk verifikasi").isVisible()){
+                break;
+            }
+
+        } while (page.getByText("Kamu belum upload dokumen wajib yang dibutuhkan pemilik kos.").isVisible());
+    }
 
     /**
      * click on Lihat Selengkapnya
@@ -109,10 +156,11 @@ public class BookingFormPO {
 
     /**
      * Increase Rent Duration
+     *
      * @throws InterruptedException
      */
     public void increaseRateDuration() throws InterruptedException {
-        if(rentDurationIncreaseButton.isVisible()) {
+        if (rentDurationIncreaseButton.isVisible()) {
             rentDurationIncreaseButton.click();
         }
     }
