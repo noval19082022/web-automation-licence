cdf8b94d3c79d9258f8784b9b2d5a51a216220ed
destafajri <>
2023-05-12 09:04:39 +0700

    uncomment codegen

--- src/main/java/codegen/Codegen.java
+++ src/main/java/codegen/Codegen.java
@@ -14,8 +14,8 @@ public class Codegen {
             context.route("**/*", route -> route.resume());
             // Pause the page, and start recording manually.
             Page page = context.newPage();
-//            page.navigate("https://jambu.kerupux.com");
-            page.navigate("https://pay-jambu.kerupux.com/pin2blkang/login");
+            page.navigate("https://jambu.kerupux.com");
+//            page.navigate("https://pay-jambu.kerupux.com/pin2blkang/login");
 //            loginAdminMamipay(page);
             //goToKostDetail(page, "Kos Wild Rift DOTF Tegalrejo Yogyakarta");
             //loginOwner(page);
