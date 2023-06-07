c95173e6ec0906fd77c4024ba7a583687d9a5e9d
Gizkara <gizkarabagas98@gmail.com>
2023-06-07 15:25:06 +0700

    no message

---
+++ src/test/java/runners/DOM3TestRunner.java
@@ -0,0 +1,12 @@
+package runners;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"json:target/result/dom2/cucumber-report.json", "html:target/result/dom2/cucumber-report.html"},
+        features = "src/test/resources/features",
+        glue = "steps",
+        tags = "@DOM3"
+)
+public class DOM3TestRunner extends BaseTestRunner {
+}
