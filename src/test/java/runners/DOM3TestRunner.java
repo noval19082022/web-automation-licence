1806d431bb499951070b62612563ebbccbce13f4
destafajri <>
2023-06-07 16:45:06 +0700

    fixing runner dom3

--- src/test/java/runners/DOM3TestRunner.java
+++ src/test/java/runners/DOM3TestRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        plugin = {"json:target/result/dom2/cucumber-report.json", "html:target/result/dom2/cucumber-report.html"},
+        plugin = {"json:target/result/dom3/cucumber-report.json", "html:target/result/dom3/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
         tags = "@DOM3"
