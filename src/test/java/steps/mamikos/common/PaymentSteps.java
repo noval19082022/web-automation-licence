57ed493c7258685e1af2272202fdcf4db1e73f72
Sakti <sakti@mamiteam.com>
2023-06-22 10:16:00 +0700

    Optimize import

--- src/test/java/steps/mamikos/common/PaymentSteps.java
+++ src/test/java/steps/mamikos/common/PaymentSteps.java
@@ -20,8 +20,6 @@ import utilities.JavaHelpers;
 import utilities.PlaywrightHelpers;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
