16a3dc2ed9f24ae181360f2d7a957b6a1d21a901
destafajri <>
2023-07-07 17:42:22 +0700

    rollback tag pipeline

--- src/test/java/runners/DOM2TestRunner.java
+++ src/test/java/runners/DOM2TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"json:target/result/dom2/cucumber-report.json", "html:target/result/dom2/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@TEST_DOM-608"
+        tags = "@DOM2"
 )
 public class DOM2TestRunner extends BaseTestRunner {
 }
