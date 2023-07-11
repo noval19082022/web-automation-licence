a582a15d4dde24f208f08ef097fbbef7c2edd0cc
destafajri <>
2023-07-11 22:45:55 +0700

    DOM-5273 Breadcrumb kost detail

--- src/test/java/steps/mamikos/common/NavigatesSteps.java
+++ src/test/java/steps/mamikos/common/NavigatesSteps.java
@@ -221,4 +221,9 @@ public class NavigatesSteps {
     public void tenantNavigateToMamipointGuidelinePage() {
         playwright.navigateTo(Mamikos.URL + Mamikos.MAMIPOIN_GUIDELINE, 30000.0, LoadState.LOAD);
     }
+
+    @When("user visit page {string}")
+    public void userVisitPage(String pathUrl) {
+        page.navigate(Mamikos.URL + pathUrl);
+    }
 }
