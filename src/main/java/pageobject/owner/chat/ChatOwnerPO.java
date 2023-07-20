5b215cc2058d4433ab27baf767ffb8cf76d19d05
upik <upik@mamiteam.com>
2023-07-20 10:38:27 +0700

    add method PO dismissFTUEMarsKuotaNol description

--- src/main/java/pageobject/owner/chat/ChatOwnerPO.java
+++ src/main/java/pageobject/owner/chat/ChatOwnerPO.java
@@ -292,6 +292,11 @@ public class ChatOwnerPO {
         playwright.clickOn(broadcastChatBtn);
     }
 
+    /**
+     * Click close FTUE on chatlist when kuota mars is 0
+     *
+     *
+     */
     public void dismissFTUEMarsKuotaNol() {
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("close")).click();
     }
