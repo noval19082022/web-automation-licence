eab9430783d553a3b5228f4ddae63f680179ddff
Sakti <sakti@mamiteam.com>
2023-08-18 15:55:47 +0700

    remove unused import

--- src/test/java/steps/api/TenantBookingApiSteps.java
+++ src/test/java/steps/api/TenantBookingApiSteps.java
@@ -14,7 +14,6 @@ import utilities.ApiPlaywrightHelpers;
 import utilities.JavaHelpers;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TenantBookingApiSteps {
