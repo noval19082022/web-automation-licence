5d3e0d3a4b78ef556a32cec05c01a7c91c004a2e
mamikos <sakti@mamiteam.com>
2023-09-07 13:28:35 +0700

    pms context browser and page

---
+++ src/main/java/config/playwright/context/PmsActiveContext.java
@@ -0,0 +1,33 @@
+package config.playwright.context;
+
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import lombok.Getter;
+import lombok.Setter;
+
+public class PmsActiveContext {
+    @Setter @Getter
+    private static BrowserContext pmsActiveBrowserContext, pmsActiveBrowserContext1;
+    @Setter @Getter
+    private static Page pmsActivePage, pmsActivePage1;
+    @Setter @Getter
+    private static String contextName;
+
+    /**
+     * Active tenant context for multiple context scenario
+     * @param pageIndex index page
+     */
+    public synchronized static void pmsActiveContext(int pageIndex) {
+        PmsActiveContext.setPmsActiveBrowserContext(PmsContext.getPmsBrowserContext());
+        PmsActiveContext.setPmsActivePage(PmsContext.getPmsBrowserContext().pages().get(pageIndex));
+    }
+
+    /**
+     * Activate owner context for multiple context scenario
+     * @param pageIndex index page
+     */
+    public synchronized static void pmsActiveContext1(int pageIndex) {
+        PmsActiveContext.setPmsActiveBrowserContext(PmsContext.getPmsBrowserContext1());
+        PmsActiveContext.setPmsActivePage1(PmsContext.getPmsBrowserContext1().pages().get(pageIndex));
+    }
+}
