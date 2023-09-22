fcdead8025115386829eac2cc45378c1927a9981
ramosan <ramosan@mamiteam.com>
2023-09-22 15:23:12 +0700

    maintain feature content mami foto

--- src/main/java/pageobject/owner/MamifotoPO.java
+++ src/main/java/pageobject/owner/MamifotoPO.java
@@ -35,6 +35,7 @@ public class MamifotoPO {
     Locator contentFAQfirstList;
     Locator riwayatPaketButton;
     Locator panduanAreaClick;
+    Locator detailTitle;
 
 
     //Locator Mamifoto at Select Package
@@ -638,6 +639,16 @@ public class MamifotoPO {
      */
     public void clickOnPanduanArea() {
         playwright.clickOn(panduanAreaClick);
+    }
+
+    /**
+     * Check title and detail title On Panduan Persiapan Foto or Video
+     * @return boolean type, appear true otherwise false
+     */
+    public boolean titleOnPanduanAndAreaAppear(String titleAndDetailText) {
+        detailTitle = page.getByText(titleAndDetailText);
+        playwright.waitTillLocatorIsVisible(detailTitle);
+        return playwright.waitTillLocatorIsVisible(detailTitle);
     }
 
 }
