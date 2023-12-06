3af31287c73bab5aa22f6feb111b25aa9f8b65f6
Bagas Gizkara <bagas.gizkara@mamiteam.com>
2023-12-06 18:52:50 +0700

    migration Selenium to playwright Change the room on Data Ketersedian Kamar

---
+++ src/main/java/pageobject/admin/mamipay/bangkrupux/AdditionalKostPO.java
@@ -0,0 +1,124 @@
+package pageobject.admin.mamipay.bangkrupux;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import utilities.PlaywrightHelpers;
+
+public class AdditionalKostPO {
+    private Page page;
+    private PlaywrightHelpers playwright;
+    Locator additionalKostMenu;
+    Locator searchTextField;
+    Locator searchButton;
+    Locator aturKetersediaanButton;
+    Locator roomStatusButton;
+    Locator updateRoomStatusButton;
+    Locator errorAlertInformationText;
+    Locator successAlertInfromationText;
+    Locator floorTextField;
+    Locator searchRoomId;
+    Locator deleteRoomButton;
+    Locator deletePopupText;
+
+
+    public AdditionalKostPO(Page page) {
+        this.page = page;
+        this.playwright = new PlaywrightHelpers(page);
+        additionalKostMenu = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Kost (Additional)"));
+        searchTextField = page.getByPlaceholder("Nama Kost");
+        searchButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(" Search"));
+        aturKetersediaanButton = page.locator("//a[contains(text(),'Atur Ketersediaan')]");
+        roomStatusButton = page.locator("//*[@class='form-group'][3]/select");
+        updateRoomStatusButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Update"));
+        errorAlertInformationText = page.getByText("Kamar dengan aktif tenant kontrak tidak bisa diubah.");
+        successAlertInfromationText = page.locator("//*[@class='alert alert-success alert-dismissable']/b");
+        floorTextField = page.locator("#floor");
+        searchRoomId= page.getByLabel("Search:");
+        deleteRoomButton = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Delete"));
+        deletePopupText = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Delete Confirmation"));
+    }
+
+    /**
+     * click menu Additional Kost
+     */
+    public void clickAdditionalKostMenu(){
+        playwright.waitTillLocatorIsVisible(additionalKostMenu);
+        playwright.clickOn(additionalKostMenu);
+
+    }
+
+    /**
+     * search kos in Kos Additional menu
+     * @param keyword kos name
+     */
+    public void searchKosAdditional(String keyword){
+        playwright.clickOn(searchTextField);
+        playwright.fill(searchTextField, keyword);
+        playwright.clickOn(searchButton);
+    }
+
+    /**
+     * click Atur Ketersediaan Button
+     */
+    public void clickButtonAturKetersediaan(){
+        playwright.waitTillLocatorIsVisible(aturKetersediaanButton);
+        playwright.clickOn(aturKetersediaanButton);
+    }
+
+    /**
+     * click Edit Button
+     *
+     */
+    public void clickOnEditButton(String name){
+        Locator editButton = page.locator("//*[@class='btn btn-sm btn-info'][@data-name=" +name+ "]");
+        playwright.clickOn(editButton);
+    }
+    /**
+     * select roomstatus and click update button
+     *
+     */
+    public void updateRoomStatusToEmpty(String updateRoom) {
+       playwright.clickOn(roomStatusButton);
+       playwright.selectDropdownByValue(roomStatusButton, updateRoom);
+       playwright.clickOn(updateRoomStatusButton);
+    }
+
+    /**
+     * Get error alert after udpate
+     */
+    public String getAlertText(String text){
+        return playwright.getText(errorAlertInformationText);
+    }
+
+    /**
+     * Get success alert after udpate
+     */
+    public String getSuccessAlertText(String text){
+      return playwright.getText(successAlertInfromationText);
+    }
+
+    /**
+     * update floor text
+     */
+    public void updateFloorName(String keyword){
+      playwright.clearText(floorTextField);
+      playwright.clickLocatorAndTypeKeyboard(floorTextField, keyword);
+      playwright.clickOn(updateRoomStatusButton);
+    }
+
+    /**
+     * click Delete  Button
+     */
+    public void clickOnDeteleButton(String text){
+        playwright.clickLocatorAndTypeKeyboard(searchRoomId, text);
+        playwright.clickOn(deleteRoomButton);
+    }
+
+    /**
+     * Get delete confirmation popup
+     */
+    public String getPopupDeleteConfirmationText() {
+        return playwright.getText(deletePopupText);
+    }
+}
---
+++ src/test/resources/features/mamikos/BBM/BBM6/editRoom.feature
@@ -0,0 +1,37 @@
+@BBM6 @ketersediaanKamar
+Feature: Change the room on Data Ketersedian Kamar
+
+  @continue @TEST_BBM-1516 @toAutomated
+  Scenario: [Data Ketersediaan Kamar][Edit Kost Room]Change the room name/number when room status is occupied with contract
+    Given admin go to mamikos bangkrupux admin
+    When admin login to bangkrupux:
+      | email stag                   | email prod                   |password  |
+      | Automation.pw1@mamikos.com   | Automation.pw1@mamikos.com   |qwerty123 |
+    And user access menu kost Additional
+    And user search "testbagas Pasawahan Kuningan" in kost additional
+    And user click on atur ketersediaan button
+    When user click the edit button in room with status is occupied with room name "1"
+    And user update room status to "Kosong" and click the update button
+    Then user can see alert "Kamar dengan aktif tenant kontrak tidak bisa diubah." on atur kamar page
+
+  @continue @TEST_BBM-1517 @toAutomated
+  Scenario: [Data Ketersediaan Kamar][Edit Kost Room]Change the room name/number when room status is occupied without contract
+    Given admin go to mamikos bangkrupux admin
+    And user access menu kost Additional
+    And user search "testbagas Pasawahan Kuningan" in kost additional
+    And user click on atur ketersediaan button
+    When user click the edit button in room with status is occupied with room name "5"
+    And user update lantai name to "2"
+    Then user can see alert "Success! Kamar berhasil di-update" on atur kamar page
+    When user click the edit button in room with status is occupied with room name "5"
+    And user update lantai name to "1"
+    Then user can see alert "Success! Kamar berhasil di-update" on atur kamar page
+
+  @continue @TEST_BBM-1515 @automated
+  Scenario: [Data Ketersediaan Kamar][Delete Kost Room]Delete button is remain locked when when room status is occupied with contract
+    Given admin go to mamikos bangkrupux admin
+    And user access menu kost Additional
+    And user search "testbagas Pasawahan Kuningan" in kost additional
+    And user click on atur ketersediaan button
+    And user search room id "330228" and delete room
+    Then user can see popup delete confirmation
