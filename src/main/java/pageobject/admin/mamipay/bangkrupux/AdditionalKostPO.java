46b4c2885e1baf98dc3debf2f52201c157662d51
Bagas Gizkara <bagas.gizkara@mamiteam.com>
2023-12-07 10:26:48 +0700

    resolve comment delete tagging

--- src/main/java/pageobject/admin/mamipay/bangkrupux/AdditionalKostPO.java
+++ src/main/java/pageobject/admin/mamipay/bangkrupux/AdditionalKostPO.java
@@ -11,14 +11,14 @@ public class AdditionalKostPO {
 
     // locator for additional kost menu Change the room on Data Ketersedian Kamar
     Locator additionalKostMenuButton;
-    Locator searchTextField;
+    Locator searchBoxNamaKost;
     Locator searchButton;
     Locator aturKetersediaanButton;
     Locator roomStatusButton;
     Locator updateRoomStatusButton;
     Locator errorAlertInformationText;
     Locator successAlertInfromationText;
-    Locator floorTextField;
+    Locator inputFieldlantai;
     Locator searchRoomId;
     Locator deleteRoomButton;
     Locator deletePopupText;
@@ -28,14 +28,14 @@ public class AdditionalKostPO {
         this.page = page;
         this.playwright = new PlaywrightHelpers(page);
         additionalKostMenuButton = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Kost (Additional)"));
-        searchTextField = page.getByPlaceholder("Nama Kost");
+        searchBoxNamaKost = page.getByPlaceholder("Nama Kost");
         searchButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(" Search"));
         aturKetersediaanButton = page.locator("//a[contains(text(),'Atur Ketersediaan')]");
         roomStatusButton = page.locator("//*[@class='form-group'][3]/select");
         updateRoomStatusButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Update"));
         errorAlertInformationText = page.getByText("Kamar dengan aktif tenant kontrak tidak bisa diubah.");
         successAlertInfromationText = page.locator("//*[@class='alert alert-success alert-dismissable']/b");
-        floorTextField = page.locator("#floor");
+        inputFieldlantai = page.locator("#floor");
         searchRoomId= page.getByLabel("Search:");
         deleteRoomButton = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Delete"));
         deletePopupText = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Delete Confirmation"));
@@ -54,8 +54,7 @@ public class AdditionalKostPO {
      * @param keyword kos name
      */
     public void searchKosAdditional(String keyword){
-        playwright.clickOn(searchTextField);
-        playwright.fill(searchTextField, keyword);
+        playwright.clickLocatorAndTypeKeyboard(searchBoxNamaKost, keyword);
         playwright.clickOn(searchButton);
     }
 
@@ -80,7 +79,6 @@ public class AdditionalKostPO {
      *
      */
     public void updateRoomStatusToEmpty(String updateRoom) {
-       playwright.clickOn(roomStatusButton);
        playwright.selectDropdownByValue(roomStatusButton, updateRoom);
        playwright.clickOn(updateRoomStatusButton);
     }
@@ -103,8 +101,8 @@ public class AdditionalKostPO {
      * update floor text
      */
     public void updateFloorName(String keyword){
-      playwright.clearText(floorTextField);
-      playwright.clickLocatorAndTypeKeyboard(floorTextField, keyword);
+      playwright.clearText(inputFieldlantai);
+      playwright.clickLocatorAndTypeKeyboard(inputFieldlantai, keyword);
       playwright.clickOn(updateRoomStatusButton);
     }
 
--- src/test/resources/features/mamikos/BBM/BBM6/editRoom.feature
+++ src/test/resources/features/mamikos/BBM/BBM6/editRoom.feature
@@ -1,7 +1,7 @@
 @BBM6 @ketersediaanKamar
 Feature: Change the room on Data Ketersedian Kamar
 
-  @continue @TEST_BBM-1516 @toAutomated
+  @TEST_BBM-1516
   Scenario: [Data Ketersediaan Kamar][Edit Kost Room]Change the room name/number when room status is occupied with contract
     Given admin go to mamikos bangkrupux admin
     When admin login to bangkrupux:
@@ -14,9 +14,12 @@ Feature: Change the room on Data Ketersedian Kamar
     And user update room status to "Kosong" and click the update button
     Then user can see alert "Kamar dengan aktif tenant kontrak tidak bisa diubah." on atur kamar page
 
-  @continue @TEST_BBM-1517 @toAutomated
+  @TEST_BBM-1517
   Scenario: [Data Ketersediaan Kamar][Edit Kost Room]Change the room name/number when room status is occupied without contract
     Given admin go to mamikos bangkrupux admin
+    When admin login to bangkrupux:
+      | email stag                   | email prod                   |password  |
+      | Automation.pw1@mamikos.com   | Automation.pw1@mamikos.com   |qwerty123 |
     And user access menu kost Additional
     And user search "testbagas Pasawahan Kuningan" in kost additional
     And user click on atur ketersediaan button
@@ -27,9 +30,12 @@ Feature: Change the room on Data Ketersedian Kamar
     And user update lantai name to "1"
     Then user can see alert "Success! Kamar berhasil di-update" on atur kamar page
 
-  @continue @TEST_BBM-1515 @automated
+  @TEST_BBM-1515
   Scenario: [Data Ketersediaan Kamar][Delete Kost Room]Delete button is remain locked when when room status is occupied with contract
     Given admin go to mamikos bangkrupux admin
+    When admin login to bangkrupux:
+      | email stag                   | email prod                   |password  |
+      | Automation.pw1@mamikos.com   | Automation.pw1@mamikos.com   |qwerty123 |
     And user access menu kost Additional
     And user search "testbagas Pasawahan Kuningan" in kost additional
     And user click on atur ketersediaan button
