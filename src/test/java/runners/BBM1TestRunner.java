b0a7d9b4e93453a8a7bf274e803c9c1cb22dc74c
Your Name <noval@mamiteam.com>
2023-12-13 12:58:53 +0700

    Update tag bbm 2

--- src/test/java/runners/BBM1TestRunner.java
+++ src/test/java/runners/BBM1TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"json:target/result/bbm1/cucumber-report.json", "html:target/result/bbm1/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@BBM1"
+        tags = "@COOP1"
 )
 public class BBM1TestRunner extends BaseTestRunner {
 }
--- src/test/java/runners/BBM2TestRunner.java
+++ src/test/java/runners/BBM2TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"json:target/result/bbm2/cucumber-report.json", "html:target/result/bbm2/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@BBM2"
+        tags = "@COOP2"
 )
 public class BBM2TestRunner extends BaseTestRunner {
 }
