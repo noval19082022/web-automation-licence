67f2f6c03ff07acdba7941bad85dce82762a936d
Mamikos <rega@mamiteam.com>
2023-12-27 12:18:39 +0700

    Maintenance LIMO: 1. Added cooment at tbc feature 2. Remove step setter getter not used at naikkanIklan.PO 3. Change locator when edit owner room group 4. Added step to dismiss FTUE , when subscribe GP from chatroom 5. Added hardwait when loading url 6. remove double step at mamiads dashboard 7. Separate feature check subscibe GP from chatroom 8. Change value when searcj contract GP 9. Remove step at pusat bantuan

--- src/main/java/pageobject/owner/TenantBackgroundCheckerPO.java
+++ src/main/java/pageobject/owner/TenantBackgroundCheckerPO.java
@@ -22,21 +22,38 @@ public class TenantBackgroundCheckerPO {
         coachmark = page.getByText("Baru: Lihat profil calon penyewa! Bagi pelanggan GoldPlus 2, Anda bisa cek info ");
         titleCoahmark = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Baru: Lihat profil calon penyewa!"));
         descCochmark = page.getByText("Bagi pelanggan GoldPlus 2, Anda bisa cek info dan riwayat pencari kos yang mengh");
-
     }
+
+    /**
+     * Check entry point TBC is visible
+     *
+     */
     public boolean isLihatProfilDisplayed() {
        return playwright.waitTillLocatorIsVisible(lihatProfilTextButton);
     }
 
+    /**
+     * Check coachmark TBC is visible
+     *
+     */
     public boolean isCoachmarkDisplayed() {
         return playwright.waitTillLocatorIsVisible(coachmark);
     }
 
+    /**
+     * get text title at coachmark tbc
+     *
+     */
     public String getTitleCoachmark(){
         return playwright.getText(titleCoahmark);
     }
 
+    /**
+     * get text description at coachmark tbc
+     *
+     */
     public String getDesCoachmark(){
         return playwright.getText(descCochmark);
     }
+
 }
--- src/test/java/steps/mamikos/owner/goldplus/GoldplusSteps.java
+++ src/test/java/steps/mamikos/owner/goldplus/GoldplusSteps.java
@@ -12,6 +12,7 @@ import pageobject.common.HomePO;
 import pageobject.common.LoadingPO;
 import pageobject.owner.OwnerDashboardPO;
 import pageobject.owner.PromoOwnerPO;
+import pageobject.owner.TenantBackgroundCheckerPO;
 import pageobject.owner.chat.ChatOwnerPO;
 import pageobject.owner.fiturpromosi.BroadcastChatPO;
 import pageobject.owner.fiturpromosi.mamiads.MamiAdsPO;
@@ -222,6 +223,7 @@ public class GoldplusSteps {
         chat.dismissFTUEMarsKuotaNol();
         broadcast.clickOnCloseTooltip();
         chat.searchChatTenant(tenantName);
+        chat.dismissFTUETBC();
         chat.clickButtonOnChatRoomList(buttonTxt);
     }
 
