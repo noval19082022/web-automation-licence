92cd20eea7aa723f037da0b8bb11ccabe8e6d72b
upras <upras@mamiteam.com>
2024-02-12 11:45:02 +0700

    New Feature Agent App menu in bangker. And consist of: 1. Table Agent Contains Role Column 2. Check is_active Yes Value When User in Edit Page 3. Check is_active No Value When User in Edit Page

---
+++ src/main/java/pageobject/admin/mamipay/bangkrupux/AgentAppPO.java
@@ -0,0 +1,102 @@
+package pageobject.admin.mamipay.bangkrupux;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import utilities.PlaywrightHelpers;
+import com.microsoft.playwright.options.AriaRole;
+
+public class AgentAppPO {
+    private Page page;
+    PlaywrightHelpers playwright;
+
+    //---Agent List Page---//
+    private Locator agentAppMenu;
+    private Locator agentsBtn;
+    private Locator columnName;
+    private Locator isActiveStatus;
+    private Locator agentInTable;
+    private Locator editBtn;
+
+    //---Edit Agent Page---//
+    private Locator isActiveStatusInEdit;
+
+    public AgentAppPO(Page page){
+        this.page = page;
+        playwright = new PlaywrightHelpers(page);
+
+        //---Agent List Page---//
+        agentAppMenu = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Agent App"));
+        agentsBtn = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Agents").setExact(true));
+    }
+
+    /**
+     * Clicks Agent App menu
+     */
+    public void clicksAgentAppMenu() {
+        playwright.clickOn(agentAppMenu);
+        playwright.clickOn(agentsBtn);
+    }
+
+    /**
+     * Get String Agent Column Table
+     * @param columnAgent
+     * @return String Agent Column Table
+     */
+    public String getAgentColumnTable(String columnAgent) {
+        columnName = page.getByRole(AriaRole.CELL, new Page.GetByRoleOptions().setName(columnAgent).setExact(true));
+        return playwright.getText(columnName);
+    }
+
+    /**
+     * Check if Is Active Status in Table = Yes
+     * True = Yes
+     * False = No
+     * @param isActive
+     * @return Is Active Status
+     */
+    public boolean isActiveStatusInTableEqualYes(String isActive) {
+        isActiveStatus = page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName(isActive).setName("Automation PMAN Yes")).locator("label");
+        return playwright.isLocatorVisibleAfterLoad(isActiveStatus, 5000.0);
+    }
+
+    /**
+     * Check if is Agent equal agent value
+     * True = equal
+     * False = not equal
+     * @param agent
+     * @return agent value
+     */
+    public boolean isAgentEqual(String agent) {
+        agentInTable = page.getByRole(AriaRole.CELL, new Page.GetByRoleOptions().setName(agent));
+        return playwright.isLocatorVisibleAfterLoad(agentInTable, 5000.0);
+    }
+
+    /**
+     * Clicks Edit button
+     * @param agent
+     */
+    public void clicksEdit(String agent) {
+        editBtn = page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName(agent)).getByRole(AriaRole.LINK);
+        playwright.clickOn(editBtn);
+    }
+
+    /**
+     * Check if Is Active Status in Edit equal isActive value
+     * True = not equal
+     * False = equal
+     * @param isActive
+     * @return Is Active Status in Edit
+     */
+    public boolean isActiveStatusInEdit(String isActive) {
+        isActiveStatusInEdit = page.locator("//select[@name='is_active']/option[@selected='true'][contains(., '" +isActive+ "')]");
+        System.out.println(isActiveStatusInEdit.getAttribute(isActive));
+        return playwright.isLocatorVisibleAfterLoad(isActiveStatusInEdit, 5000.0);
+    }
+
+    /**
+     * Go back to previous page
+     */
+    public void goBackToPreviousPage() {
+        playwright.backToPreviousPage();
+    }
+}
---
+++ src/test/resources/features/mamikos/PMAN/agentApp/agentApp.feature
@@ -0,0 +1,29 @@
+@regression @pman @agentApp
+
+Feature: Agent App
+
+  @TEST_PMAN-9307 @pman-prod @continue
+  Scenario: Table Agent Contains Role Column
+    Given admin go to mamikos bangkrupux admin
+    When admin login to bangkrupux:
+      | email stag                    | email prod                    | password        |
+      | automationpman01@mamikos.com  | automationpman01@mamikos.com  | qwerty123       |
+    And admin goes to Agent App menu
+    Then agent column is contains
+      | ID        |
+      | Code      |
+      | Type      |
+      | Agent     |
+      | Role      |
+      | Is active |
+      | Action    |
+
+  @TEST_PMAN-9382 @continue
+  Scenario: Check is_active Yes Value When User in Edit Page
+    When admin checks is active "Yes" status in Edit page for "Automation PMAN Yes" agent
+    Then is active status in Edit page is "Yes"
+
+  @TEST_PMAN-9422
+  Scenario: Check is_active No Value When User in Edit Page
+    When admin checks is active "No" status in Edit page for "Automation PMAN No" agent
+    Then is active status in Edit page is "No"
