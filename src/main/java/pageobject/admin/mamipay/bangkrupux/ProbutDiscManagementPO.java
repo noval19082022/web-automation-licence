41076c92266ac7ab02cc0f9e1508e7fa5b907892
Desta Fajri <destafajri@gmail.com>
2024-03-29 02:10:25 +0700

    Refactor and create new Steps For Remove bulk discount management

--- src/main/java/pageobject/admin/mamipay/bangkrupux/ProbutDiscManagementPO.java
+++ src/main/java/pageobject/admin/mamipay/bangkrupux/ProbutDiscManagementPO.java
@@ -11,21 +11,35 @@ public class ProbutDiscManagementPO {
     private PlaywrightHelpers playwrightHelpers;
     private Locator bulkActionBtn;
     private Locator uploadBtn;
-    private Locator choosenFileCsvInput;
+    private Locator removeBtn;
+    private Locator choosenFileCsvInputForUpload;
+    private Locator choosenFileCsvInputForRemove;
     private Locator uploadBulkBtn;
 
     public ProbutDiscManagementPO(Page page) {
         this.playwrightHelpers = new PlaywrightHelpers(page);
         this.bulkActionBtn = page.getByRole(AriaRole.BUTTON).getByText("Bulk Action");
         this.uploadBtn = page.locator("a[data-target='#bulkUpload']");
-        this.choosenFileCsvInput = page.locator("#bulkUploadInput");
+        this.removeBtn = page.locator("a[data-target='#bulkRemove']");
+        this.choosenFileCsvInputForUpload = page.locator("#bulkUploadInput");
+        this.choosenFileCsvInputForRemove = page.locator("#bulkRemoveInput");
         this.uploadBulkBtn = page.locator("#bulkUploadButton");
     }
 
     public void uploadBulkDiscountCsv(String fileName) {
         playwrightHelpers.clickOn(bulkActionBtn);
         playwrightHelpers.clickOn(uploadBtn);
-        choosenFileCsvInput.setInputFiles(Paths.get("src/main/resources/file/bulk-probut-discount-management/" + fileName));
+        choosenFileCsvInputForUpload.setInputFiles(
+                Paths.get("src/main/resources/file/bulk-probut-discount-management/upload/" + fileName));
+        playwrightHelpers.hardWait(500.0);
+        playwrightHelpers.clickOn(uploadBulkBtn);
+    }
+
+    public void removeBulkDiscountCsv(String fileName) {
+        playwrightHelpers.clickOn(bulkActionBtn);
+        playwrightHelpers.clickOn(removeBtn);
+        choosenFileCsvInputForRemove.setInputFiles(
+                Paths.get("src/main/resources/file/bulk-probut-discount-management/remove/" + fileName));
         playwrightHelpers.hardWait(500.0);
         playwrightHelpers.clickOn(uploadBulkBtn);
     }
