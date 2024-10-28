5e4fd42b7ac203f43d391475b98291c233e05593
indratouwe <sakti@mamiteam.com>
2024-10-28 17:23:07 +0700

    optimism import

--- src/main/java/pageobject/common/KostDetailsPO.java
+++ src/main/java/pageobject/common/KostDetailsPO.java
@@ -4,7 +4,6 @@ import com.microsoft.playwright.ElementHandle;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import config.global.GlobalConfig;
 import pageobject.tenant.BookingFormPO;
 import utilities.JavaHelpers;
 import utilities.LocatorHelpers;
