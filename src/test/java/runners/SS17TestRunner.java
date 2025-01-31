88ecb69400c1b1ad0ecf826dacf4b7af8fa9b257
yudha <yudha@mamiteam.com>
2025-01-31 16:19:31 +0700

    change target

--- src/test/java/runners/SS17TestRunner.java
+++ src/test/java/runners/SS17TestRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        plugin = {"json:target/result/ss16/cucumber-report.json", "html:target/result/ss16/cucumber-report.html"},
+        plugin = {"json:target/result/ss17/cucumber-report.json", "html:target/result/ss16/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
         tags = "@SS17"
