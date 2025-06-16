49090c1b8c60c7c9f8b71193f1a419db73a5c265
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2025-06-16 17:26:00 +0700

    LIMO-9073 maintenance limo

--- src/test/java/steps/mamikos/owner/propertisaya/PropertiSayaSteps.java
+++ src/test/java/steps/mamikos/owner/propertisaya/PropertiSayaSteps.java
@@ -662,10 +662,17 @@ public class PropertiSayaSteps {
     @Then("user see kos with valid name, status {string} and type {string}")
     public void userSeeKosWithValidNameStatusAndType(String status, String kosType) {
         propertySaya.waitPageLoaded();
-        Assert.assertTrue(propertySaya.getFirstKosName().equalsIgnoreCase(Mamikos.getPropertyKosName()),
+        Assert.assertTrue(propertySaya.
+                        getFirstKosName().
+                        toLowerCase().
+                        contains(
+                                Mamikos.
+                                        getPropertyKosName().
+                                        toLowerCase().replace("kos", "kost")
+                        ),
                 "Kos name is wrong"
-                        + "expected contains: " + Mamikos.getPropertyKosName()
-                        + "actual: " + propertySaya.getFirstKosName());
+                        + " expected contains: " + Mamikos.getPropertyKosName()
+                        + " actual: " + propertySaya.getFirstKosName());
         Assert.assertTrue(propertySaya.getFirstKosStatus(status).contains(status), "Kos name field is still enable");
         Assert.assertEquals(propertySaya.getFirstKosType(kosType), kosType, "Kos type is wrong");
     }
