21d39b8aea4f4355de8be29328eec062653a34e2
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2025-10-18 12:56:00 +0700

    LIMO-9577 [Owner][GPSP][OD] Display correct GP card variant for each segment

--- src/test/java/steps/mamikos/owner/goldplus/GoldplusSteps.java
+++ src/test/java/steps/mamikos/owner/goldplus/GoldplusSteps.java
@@ -890,7 +890,7 @@ public class GoldplusSteps {
         Assert.assertEquals(actualCopyText, expectedCopyText, "Copy text doesn't match. Expected: " + expectedCopyText + " but found: " + actualCopyText);
     }
 
-    @Then("user check countdown value running")
+    @Then("user/owner check countdown value running")
     public void userCheckCountdownValueRunning() throws InterruptedException {
         // Get initial countdown value
         String initialCountdown = goldplus.getCountdownTimerValue();
@@ -923,5 +923,10 @@ public class GoldplusSteps {
     public void userCheckNoCountdownValueRunning() {
         Assert.assertFalse(goldplus.isCountdownTimerDisplayed(), "Countdown timer should not be displayed, but it is visible on the page");
         System.out.println("Confirmed: No countdown timer is running on the page");
+    }
+
+    @And("owner see GPSP promo countdown")
+    public void ownerSeeGPSPPromoCountdown() {
+        Assert.assertTrue(owner.isGpspPromoCountDownExist(), "gpsp promo countdown is not exist on owner dashboard");
     }
 }
