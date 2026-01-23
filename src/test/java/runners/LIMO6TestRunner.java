bff65a5608bdcafdfc3fe6cea020cb847ea3c478
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2026-01-23 15:01:24 +0700

    LIMO-9897 Maintenance owner

--- src/test/java/runners/LIMO6TestRunner.java
+++ src/test/java/runners/LIMO6TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"json:target/result/limo6/cucumber-report.json", "html:target/result/limo6/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@LIMO6"
+        tags = "@samedaySurveyTenant"
 )
 public class LIMO6TestRunner extends BaseTestRunner{
 }
